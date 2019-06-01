9801cc9f97913a2871c5a9b8c3389b9ec2d9c480
Jonathan Tafel <tafelito@gmail.com>
2019-06-01 17:36:37 -0400

    add star widget

--- lib/widgets/review_widget.dart
+++ lib/widgets/review_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutterhack/data/boelens_bloc_provider.dart';
 import 'package:flutterhack/data/review_model.dart';
 import 'package:flutterhack/data/user_bloc.dart';
@@ -12,14 +13,29 @@ class ReviewWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final bloc = UserBloc(userId: review.posterId);
     return Column(children: <Widget>[
-      Container(width: double.infinity, 
-              child: BoelensBlocProvider<UserBloc>(
+      Container(
+        width: double.infinity,
+        child: BoelensBlocProvider<UserBloc>(
           bloc: bloc,
           child: UserWidget(),
         ),
       ),
       Container(width: double.infinity, child: Text('Post date:  ${review.postDate}')),
-      Container(width: double.infinity, child: Text('Rating:  ${review.rating}')),
+      Container(
+          width: double.infinity,
+          child: Row(
+            children: <Widget>[
+              Text('Rating'),
+              SizedBox(width: 10),
+              FlutterRatingBarIndicator(
+                rating: review.rating.toInt().toDouble(),
+                itemCount: 5,
+                itemSize: 10.0,
+                fillColor: Colors.indigo,
+                emptyColor: Colors.amber.withAlpha(40),
+              )
+            ],
+          )),
       Container(width: double.infinity, child: Text('Difficulty:  ${review.reportedDifficulty}')),
       Container(width: double.infinity, child: Text('Review:  ${review.review}')),
     ]);
