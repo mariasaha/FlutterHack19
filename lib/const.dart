c16be91b2aa81731c40b74cbca64ebdf9d9c59cd
Tim Reynolds <tmrdev@gmail.com>
2019-06-01 14:01:12 -0400

    - adding constant class

---
+++ lib/const.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+
+// blue
+final themeColor = new Color(0xFF42A5F5);
+//final themeColor = Color.fromRGBO(243, 241, 150, 1);
+//final themeColor = Color.fromRGBO(255, 51, 51, 1);
+// original tan or whatever it is!
+//final themeColor = new Color(0xfff5a623);
+final primaryColor = new Color(0xff203152);
+final greyColor = new Color(0xffaeaeae);
+final greyColor2 = new Color(0xffE8E8E8);
+final testColor = new Color(0xFFb74093);
+
+// using const or constant for chat text limit int below may have been causing issues and never set the limit
+// changing from const to final for now and tested that it works
+final int CHAT_TEXT_LIMIT = 150;
--- lib/login.dart
+++ lib/login.dart
@@ -1,24 +1,27 @@
 import 'dart:async';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'const.dart';
 
+
+///import 'package:source-tree/const.dart';
+///import 'package:flutter_chat_demo/main.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-
-
+//import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Chat Demo',
-      theme: new ThemeData(
-        /// Theme color set statically here, add constant
-        primaryColor: Colors.blue,
+      theme: ThemeData(
+        primaryColor: themeColor,
       ),
-      home: LoginScreen(title: 'Flutter Chat'),
+      home: LoginScreen(title: 'CHAT DEMO'),
       debugShowCheckedModeBanner: false,
     );
   }
@@ -30,21 +33,17 @@ class LoginScreen extends StatefulWidget {
   final String title;
 
   @override
-  LoginScreenState createState() => new LoginScreenState();
+  LoginScreenState createState() => LoginScreenState();
 }
 
 class LoginScreenState extends State<LoginScreen> {
-  final GoogleSignIn googleSignIn = new GoogleSignIn();
+  final GoogleSignIn googleSignIn = GoogleSignIn();
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   SharedPreferences prefs;
 
   bool isLoading = false;
   bool isLoggedIn = false;
   FirebaseUser currentUser;
-  String currentUserFCMToken;
-
-  // for Firebase Chat Notifications
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
   @override
   void initState() {
@@ -52,7 +51,6 @@ class LoginScreenState extends State<LoginScreen> {
     isSignedIn();
   }
 
-  /// isSignedIn - check if user is logged in
   void isSignedIn() async {
     this.setState(() {
       isLoading = true;
@@ -62,16 +60,11 @@ class LoginScreenState extends State<LoginScreen> {
 
     isLoggedIn = await googleSignIn.isSignedIn();
     if (isLoggedIn) {
-      getCurrentUserFCMToken();
-      print("** isSignedIn :: fcm token ->> :: > ");
       Navigator.push(
         context,
-        MaterialPageRoute(
-            builder: (context) =>
-                MainScreen(currentUserId: prefs.getString('id'))),
+        //MaterialPageRoute(builder: (context) => MyApp(currentUserId: prefs.getString('id'))),
+        MaterialPageRoute(builder: (context) => MyApp()),
       );
-    } else {
-      print("** isSignedIn -- isLoggedIn false");
     }
 
     this.setState(() {
@@ -79,111 +72,60 @@ class LoginScreenState extends State<LoginScreen> {
     });
   }
 
-  String getCurrentUserFCMToken() {
-    _firebaseMessaging.getToken().then((token){
-      print("** getCurrentUserFCMToken :: fcm token ->> :: > " + token);
-      return token.toString();
-    }, onError: (err) {
-      /// Could not get fcm token for user
-      print("** getCurrentUserFCMToken :: token error");
-      return null;
-    });
-  }
-
-  /// Future handleSignIn - call all sign ins be handled here
   Future<Null> handleSignIn() async {
-    print("** handleSignIn top");
     prefs = await SharedPreferences.getInstance();
 
     this.setState(() {
       isLoading = true;
     });
 
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
 
-    // Init firebaseUser 
-    FirebaseUser firebaseUser;
-    try{
-      /// sign in user to Google
-      final GoogleSignInAccount googleUser = await googleSignIn.signIn();
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
 
-      final AuthCredential credential = GoogleAuthProvider.getCredential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-
-      firebaseUser = await firebaseAuth.signInWithCredential(credential);
-      print("** handleSignIn -- signed in " + firebaseUser.displayName);
-    
-    } catch (e) {
-      print("** exception -> ** " + e.toString());
-      throw Error();
-    }
+    FirebaseUser firebaseUser = await firebaseAuth.signInWithCredential(credential);
 
     if (firebaseUser != null) {
-      // Check is User already signed in?
-      print("** handleSignIn firebaseUser is NOT NULL");
-
-      final QuerySnapshot result = await Firestore.instance
-          .collection('users')
-          .where('id', isEqualTo: firebaseUser.uid)
-          .getDocuments();
+      // Check is already sign up
+      final QuerySnapshot result =
+      await Firestore.instance.collection('users').where('id', isEqualTo: firebaseUser.uid).getDocuments();
       final List<DocumentSnapshot> documents = result.documents;
       if (documents.length == 0) {
         // Update data to server if new user
-        print("** handleSignIn -- New user, save profile data to Firestore instance ***");
-        // NOTE: Adding fcm token to user data on firestore so that cloud functions can send notifications
-        _firebaseMessaging.getToken().then((token){
-          print("** iOS :: fcm token ->> :: > " + token);
-          Firestore.instance
-              .collection('users')
-              .document(firebaseUser.uid)
-              .setData({
-            'nickname': firebaseUser.displayName,
-            'photoUrl': firebaseUser.photoUrl,
-            'email': firebaseUser.email,
-            'fcmtoken' : token,
-            'id': firebaseUser.uid
-          });
-        }, onError: (err) {
-          /// Could not get fcm token for user
-          Fluttertoast.showToast(msg: 'User login error');
-        });
+        Firestore.instance
+            .collection('users')
+            .document(firebaseUser.uid)
+            .setData({'nickname': firebaseUser.displayName, 'photoUrl': firebaseUser.photoUrl, 'id': firebaseUser.uid});
 
         // Write data to local
         currentUser = firebaseUser;
         await prefs.setString('id', currentUser.uid);
         await prefs.setString('nickname', currentUser.displayName);
         await prefs.setString('photoUrl', currentUser.photoUrl);
-        await prefs.setString('email', currentUser.email);
       } else {
-        print("** handleSignIn -- User has already logged in, profile data should already be saved in Firestore ***");
         // Write data to local
-        // TODO: Look into issue with About Me not updating text
-        // Also may need to update fcm token on firestore database, need to make sure fcm token is always up to date
         await prefs.setString('id', documents[0]['id']);
         await prefs.setString('nickname', documents[0]['nickname']);
         await prefs.setString('photoUrl', documents[0]['photoUrl']);
         await prefs.setString('aboutMe', documents[0]['aboutMe']);
-        await prefs.setString('email', documents[0]['email']);
-
       }
       Fluttertoast.showToast(msg: "Sign in success");
       this.setState(() {
         isLoading = false;
       });
 
-      print("** handleSignIn before Navigator push");
       Navigator.push(
         context,
         MaterialPageRoute(
             builder: (context) => MainScreen(
-                  currentUserId: firebaseUser.uid,
-                )),
+              currentUserId: firebaseUser.uid,
+            )),
       );
-      print("** handleSignIn AFTER Navigator push");
     } else {
-      print("** handleSignIn Sign In Failed");
       Fluttertoast.showToast(msg: "Sign in fail");
       this.setState(() {
         isLoading = false;
@@ -191,16 +133,13 @@ class LoginScreenState extends State<LoginScreen> {
     }
   }
 
-  /// Widget build - AppBar config and User Sign In Buttons
-  ///
-  /// calls handleSignIn
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(
             widget.title,
-            style: TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+            style: TextStyle(color: primaryColor, fontWeight: FontWeight.bold),
           ),
           centerTitle: true,
         ),
@@ -224,13 +163,13 @@ class LoginScreenState extends State<LoginScreen> {
             Positioned(
               child: isLoading
                   ? Container(
-                      child: Center(
-                        child: CircularProgressIndicator(
-                          valueColor: AlwaysStoppedAnimation<Color>(Colors.blue),
-                        ),
-                      ),
-                      color: Colors.white.withOpacity(0.8),
-                    )
+                child: Center(
+                  child: CircularProgressIndicator(
+                    valueColor: AlwaysStoppedAnimation<Color>(themeColor),
+                  ),
+                ),
+                color: Colors.white.withOpacity(0.8),
+              )
                   : Container(),
             ),
           ],
